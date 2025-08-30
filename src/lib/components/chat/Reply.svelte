7f900eb0a42cee18cd2d4f4a6208bba9dea283a7
Mauznemo <mauznemo@gmail.com>
2025-08-30 17:26:59 +0200

    feat: better handling of decryption fails

--- src/lib/components/chat/Reply.svelte
+++ src/lib/components/chat/Reply.svelte
@@ -15,12 +15,17 @@
 {#if replyToMessage?.replyTo}
 	<div class="mb-1 rounded-sm border-l-2 border-white/50 bg-gray-500/50 pr-1 pb-0.5 pl-2">
 		<svelte:boundary>
-			<p class="line-clamp-4 max-w-[40ch] text-sm break-words whitespace-pre-line text-gray-100">
-				{@html processLinks(await decryptMessage(replyToMessage.replyTo.encryptedContent, chatKey))}
-			</p>
-			{#snippet pending()}
-				<p class="whitespace-pre-line text-white">loading...</p>
-			{/snippet}
+			{#await decryptMessage(replyToMessage.replyTo.encryptedContent, chatKey)}
+				<p class="line-clamp-4 max-w-[40ch] text-sm break-words whitespace-pre-line text-gray-100">
+					loading...
+				</p>
+			{:then decryptedContent}
+				<p class="line-clamp-4 max-w-[40ch] text-sm break-words whitespace-pre-line text-gray-100">
+					{@html processLinks(decryptedContent)}
+				</p>
+			{:catch error}
+				<p class="pr-9 whitespace-pre-line text-red-300">Failed to load message</p>
+			{/await}
 		</svelte:boundary>
 	</div>
 {/if}
