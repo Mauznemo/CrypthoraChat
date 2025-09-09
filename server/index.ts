4592a6b8bed70d7461b1c937f7b33f1d3cf21bd7
Mauznemo <mauznemo@gmail.com>
2025-09-09 12:39:53 +0200

    fix: docker issues

--- server/index.ts
+++ server/index.ts
@@ -7,16 +7,33 @@ import cors from 'cors';
 const app = express();
 const server = http.createServer(app);
 
-// Add CORS middleware
+// app.use((req, res, next) => {
+// 	console.log('TRACE INCOMING ->', req.method, req.url, 'origin=', req.headers.origin);
+
+// 	// capture stack when response ends with error status
+// 	const originalEnd = res.end;
+// 	res.end = function (...args: any[]) {
+// 		// log status at the moment of end
+// 		console.log(`TRACE RESPONSE -> ${req.method} ${req.url} status=${res.statusCode}`);
+// 		if (res.statusCode >= 400) {
+// 			console.log('TRACE: printing stack (where response ended).');
+// 			// print a stack to show the code path
+// 			console.trace();
+// 		}
+// 		return originalEnd.apply(this, args as any);
+// 	};
+
+// 	next();
+// });
+
+// CORS middleware
 app.use(
 	cors({
-		origin:
-			process.env.NODE_ENV === 'production'
-				? ['https://chat.mauznemo-server.de'] // Replace with your actual production domain
-				: 'http://localhost:5173', // Dev server URL
+		origin: process.env.NODE_ENV === 'production' ? process.env.CHAT_URL : 'http://localhost:3000', // Dev server URL
 		credentials: true
 	})
 );
+
 initializeSocket(server);
 
 // SvelteKit handlers
