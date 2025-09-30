4ab8588edee027fc224ebf13b9bd3ba77ce62a85
Mauznemo <mauznemo@gmail.com>
2025-09-30 23:54:20 +0200

    fix: replying to message with only attachments

--- src/lib/components/chat/Reply.svelte
+++ src/lib/components/chat/Reply.svelte
@@ -2,6 +2,7 @@
 	import { processMessageText } from '$lib/chat/textTools';
 	import { decryptMessage } from '$lib/crypto/message';
 	import type { ClientMessage } from '$lib/types';
+	import Icon from '@iconify/svelte';
 
 	const {
 		replyToMessage
@@ -18,9 +19,15 @@
 					loading...
 				</p>
 			{:then decryptedContent}
-				<p class="line-clamp-4 max-w-[40ch] text-sm break-words whitespace-pre-line text-gray-100">
-					{@html processMessageText(decryptedContent)}
-				</p>
+				{#if decryptedContent === ''}
+					<Icon icon="mdi:attach-file" class="size-5 pt-0.5 text-gray-100" />
+				{:else}
+					<p
+						class="line-clamp-4 max-w-[40ch] text-sm break-words whitespace-pre-line text-gray-100"
+					>
+						{@html processMessageText(decryptedContent)}
+					</p>
+				{/if}
 			{:catch error}
 				<p class="pr-9 whitespace-pre-line text-red-300">Failed to load message {error}</p>
 			{/await}
